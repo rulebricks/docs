8e00df1fab97abbcea5146e061d0e056564a0f9b
Siddharth Garimella <gsidsid@gmail.com>
2024-05-02 11:14:35 -0700

    update

--- components/NavbarLogo.tsx
+++ components/NavbarLogo.tsx
@@ -2,7 +2,7 @@ function NavbarLogo(props) {
   return (
     <div className="inline-flex align-middle cursor-pointer my-1.5">
       <img
-        className={'ml-1 sm:ml-2 mr-3.5 h-6 self-center nav-logo invert'}
+        className={'ml-1 sm:ml-2 mr-3.5 h-6 self-center nav-logo invert-[0.3]'}
         src="https://d1zic6dm9txw4h.cloudfront.net/rulebricks-docs-assets/static/images/application/logo.png"
         alt="Rulebricks Logo"
       />
